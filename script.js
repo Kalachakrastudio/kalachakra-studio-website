85f553b0977f3db7126c8805f0230d078d536151
Kalachakrastudio <kalachakrastudio@gmail.com>
2026-02-16 21:19:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 // MOBILE MENU
-const toggle = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
+document.addEventListener("DOMContentLoaded", function () {
 
-toggle.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
+  const toggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  toggle.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+  });
+
+  // HERO SLIDER
+  const slides = document.querySelectorAll(".slide");
+  let index = 0;
+
+  function showSlides() {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[index].classList.add("active");
+    index++;
+    if (index >= slides.length) index = 0;
+  }
+
+  if (slides.length > 0) {
+    showSlides();
+    setInterval(showSlides, 4000);
+  }
+
 });
-
-// HERO SLIDESHOW
-let slides = document.querySelectorAll(".slide");
-let index = 0;
-
-function showSlide() {
-  slides.forEach((slide) => slide.classList.remove("active"));
-  slides[index].classList.add("active");
-  index = (index + 1) % slides.length;
-}
-
-setInterval(showSlide, 4000);
-showSlide();
