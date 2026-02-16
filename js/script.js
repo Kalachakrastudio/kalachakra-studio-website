cfb9b8faefdcc76c9f004bce8a9885b22307fac0
Kalachakrastudio <kalachakrastudio@gmail.com>
2026-02-16 22:54:48 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,35 +1,6 @@
-window.onload = function(){
-
 const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("nav-menu");
-const closeBtn = document.getElementById("closeBtn");
-const overlay = document.getElementById("menuOverlay");
+const nav = document.getElementById("nav-menu");
 
-if(!hamburger || !navMenu || !closeBtn){
-    console.log("Menu elements not found");
-    return;
-}
-
-/* OPEN MENU */
-hamburger.onclick = function(){
-    navMenu.classList.add("active");
-    overlay.classList.add("active");
-    document.body.style.overflow = "hidden";
-};
-
-/* CLOSE MENU */
-function closeMenu(){
-    navMenu.classList.remove("active");
-    overlay.classList.remove("active");
-    document.body.style.overflow = "auto";
-}
-
-closeBtn.onclick = closeMenu;
-overlay.onclick = closeMenu;
-
-/* CLOSE WHEN LINK CLICKED */
-console.log("JS Loaded");
-
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("active");
 });
-
-};
