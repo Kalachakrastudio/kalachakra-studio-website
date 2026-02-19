b9c17b736886bb0a93f293009789c12ffc8205d7
Kalachakrastudio <kalachakrastudio@gmail.com>
2026-02-19 23:54:01 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,12 @@
-/*const hamburger = document.getElementById("hamburger");
-const nav = document.getElementById("nav-menu");
-
-hamburger.addEventListener("click", () => {
-  nav.classList.toggle("active");
-});*/
-
 document.addEventListener("DOMContentLoaded", function () {
 
   const hamburger = document.getElementById("hamburger");
-  const nav = document.getElementById("nav");
-  const links = document.querySelectorAll(".nav a");
+  const nav = document.getElementById("nav-menu");
 
-  // Safety check (prevents null errors)
-  if (!hamburger || !nav) {
-    console.error("Hamburger or Nav not found in HTML");
-    return;
-  }
+  if (!hamburger || !nav) return;
 
   hamburger.addEventListener("click", function () {
     nav.classList.toggle("active");
   });
 
-  links.forEach(function (link) {
-    link.addEventListener("click", function () {
-      nav.classList.remove("active");
-    });
-  });
-
 });
-
